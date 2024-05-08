11067d492a17c02c6c360e8f2b9fd9e01aa377ee
SanjivJais <sanjivjaiswal04@gmail.com>
2024-05-08 09:12:16 +0545

    Announcements page completed

---
+++ src/components/AnnouncementsCard.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import ReactHtmlParser from 'react-html-parser'
+
+export const AnnouncementsCard = ({ announcement }) => {
+
+    const formatDateTime = (dateTimeString) => {
+        // Split the input string into date and time parts
+
+        const [datePart, timePart] = dateTimeString.split('T');
+
+        const [year, month, day] = datePart.split('-');
+        const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+        const formattedDate = `${months[parseInt(month, 10) - 1]} ${parseInt(day, 10)}, ${year}`;
+
+        // Parse the time part
+        const [hours, minutes] = timePart.split(':');
+        const hour = parseInt(hours, 10) > 12 ? parseInt(hours, 10) - 12 : parseInt(hours, 10);
+        const ampm = parseInt(hours, 10) >= 12 ? 'PM' : 'AM';
+        const formattedTime = `${hour}:${minutes} ${ampm}`;
+
+        return { date: formattedDate, time: formattedTime };
+    };
+
+
+    return (
+        <>
+            <div className='bg-secondary p-6 gap-3 flex max-sm:flex-col w-full my-5 rounded-[5px] '>
+                <div className="flex md:flex-col md:gap-[3px] gap-[6px] text-inactive min-w-24">
+                    <div>Published On</div>
+                    <div className='text-offBlue'>{formatDateTime(announcement.$createdAt).date}</div>
+                </div>
+                <div className="w-[0.8px] h-full bg-inactive bg-opacity-20">
+
+                </div>
+                <div className="flex flex-col gap-1">
+                    <h3 className='text-[1.3rem] font-semibold text-offWhite'>{announcement.title}</h3>
+
+                    <div className='text-offBlue content-area'>{ReactHtmlParser(announcement.content)}</div>
+                </div>
+            </div>
+        </>
+    )
+}
--- src/pages/Announcements.jsx
+++ src/pages/Announcements.jsx
@@ -3,16 +3,42 @@ import { Helmet } from 'react-helmet'
 import LoadingBar from 'react-top-loading-bar'
 import Breadcrumbs from '../components/Breadcrumbs'
 import { GrAnnounce } from 'react-icons/gr'
+import { AnnouncementsCard } from '../components/AnnouncementsCard'
+import { toast } from 'react-toastify'
+import { ID, database, db_id } from '../../config/Appwrite'
+import { Query } from 'appwrite'
+
 
 
 export const Announcements = () => {
 
   const [progress, setProgress] = useState(0)
+
+  const tabs = document.getElementsByClassName("announcementTab");
+  let [activeTab, setActiveTab] = useState(0);
+  const handleTabs = (event) => {
+    const index = Array.from(tabs).indexOf(event.target);
+    setActiveTab(index);
+  }
+
+  useEffect(() => (
+    setActiveTab(activeTab)
+  ), [activeTab])
+
+
+  const [announcements, setAnnouncements] = useState([])
   useEffect(() => {
-    setProgress(50)
-    setTimeout(() => {
-      setProgress(100)
-    }, 1000)
+    const fetchAnnouncements = async () => {
+      setProgress(50)
+      try {
+        const response = await database.listDocuments(db_id, 'announcements', [Query.orderDesc('$createdAt')])
+        setAnnouncements(response.documents)
+      } catch (error) {
+        toast.error("Something went wrong!")
+      }
+    }
+    fetchAnnouncements();
+    setProgress(100)
   }, [])
 
   return (
@@ -29,8 +55,46 @@ export const Announcements = () => {
         <h1 className='text-3xl font-bold text-offWhite flex gap-2 items-center'><GrAnnounce /><span>Announcements</span></h1>
       </div>
 
-      <div className="flex flex-col min-h-[420px]">
+      <div className="flex flex-col min-h-[420px] w-full max-w-[800px] px-4 self-center mt-6 items-start">
+        <div className="flex max-md:justify-between md:gap-6 gap-4 text-base custom-scrollbar whitespace-nowrap overflow-x-auto">
+          <div onClick={(e) => handleTabs(e)} className={`announcementTab ${activeTab === 0 ? 'md:border-b-2 md:border-primary md:text-offBlue text-primary' : 'text-inactive hover:text-offBlue'}  pb-1 font-semibold cursor-pointer`}>Latest</div>
+          <div onClick={(e) => handleTabs(e)} className={`announcementTab ${activeTab === 1 ? 'md:border-b-2 md:border-primary md:text-offBlue text-primary' : 'text-inactive hover:text-offBlue'}  pb-1 font-semibold cursor-pointer`}>Featured</div>
+        </div>
 
+        {activeTab === 0 &&
+          <div className='md:mt-3'>
+            {announcements.length > 0 ?
+              <>
+                {announcements.map((announcement, index) => (
+                  <AnnouncementsCard key={index} announcement={announcement} />
+                ))}
+              </>
+              :
+              <>
+                <div className='text-inactive font-semibold text-md'>
+                  All recent announcements will show up here!
+                </div>
+              </>
+            }
+          </div>
+        }
+        {activeTab === 1 &&
+          <div className='md:mt-3'>
+            {announcements.filter((announcement)=>announcement.isFeatured).length > 0 ?
+              <>
+                {announcements.filter((announcement)=>announcement.isFeatured).map((announcement, index) => (
+                  <AnnouncementsCard key={index} announcement={announcement} />
+                ))}
+              </>
+              :
+              <>
+                <div className='text-inactive font-semibold text-md'>
+                  All featured announcements will show up here!
+                </div>
+              </>
+            }
+          </div>
+        }
       </div>
 
     </>
